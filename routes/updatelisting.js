99aadf5c9c90d6bc23e61eb51947699ac2f6c3ab
Anu Slorah <anuslorah@hotmail.com>
2019-03-04 17:52:32 -0800

    started db work

--- routes/updatelisting.js
+++ routes/updatelisting.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('../db/connection');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('updatelisting', { title: 'Update a bar' });
+
+
+///* GET home page. */
+//router.get('/', function(req, res, next) {
+//  res.render('updatelisting', { title: 'Update a bar' });
+//});
+//
+//module.exports = router;
+//
+//****************************
+//router.get('/:id', function(req, res, next) {
+//  var sqlQuery = "select * from film where film_id=?";
+//  db.query(sqlQuery, [req.params.id], (error, result, fields) => {
+//    if (error) {
+//      res.status(500).send(error);
+//    }
+//    res.send(result);
+//  });
+//});
+//
+//module.exports = router;
+//*************
+//
+///* GET home page. */
+//router.get('/', function(req, res, next) {
+//  //res.render('index', { title: 'Express' });
+//  var sqlQuery = 'SELECT * FROM besthappy';
+//  mysql.connection.connect();
+//  mysql.connection.query(sqlQuery, function (error, results, fields) {
+//    if (error) throw error;
+//    res.send(JSON.stringify(results[0]));
+//  });
+//  mysql.connection.end();
+//
+//});
+router.get('/all', function(req, res, next) {
+  db.query("select * from besthappy", (error, result, fields) => {
+    if (error) {
+      res.status(500).send(error);
+    }
+    res.send(result);
+  });
 });
-
 module.exports = router;
